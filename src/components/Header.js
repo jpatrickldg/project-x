1d97e1fcd9e9192aca1ab8b33a2d0238bfe57f57
jpatrickldg <jpatrickldg@gmail.com>
2022-11-28 13:32:23 +0800

    Adjust header height

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="flex items-center justify-between h-20 text-white">
+    <header className="flex items-center justify-between h-16 text-white">
       <h1 className="text-2xl md:text-3xl font-bold tracking-wider">Project<span className="text-blue-500 font-extrabold">X</span></h1>
     </header>
   )
